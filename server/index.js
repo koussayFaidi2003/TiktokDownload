cdd52c1fe0522589b05d30a0666ea47b8ae35926
koussayFaidi2003 <koussay.faidi@esprit.tn>
2026-02-12 02:37:31 +0100

    back deploy

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // Health check
